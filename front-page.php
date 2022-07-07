bafc44515278d1dac1197448fe6f53e7c596420d
serii <seriiburduja@gmail.com>
2022-07-07 18:29:16 +0300

    feat: body bg

--- front-page.php
+++ front-page.php
@@ -4,8 +4,11 @@
  * Template Name: Home
  */
 get_header(); ?>
-<?php require_once __DIR__ . '/template-parts/home-intro.php'; ?>
-<?php require_once __DIR__ . '/template-parts/progetto.php'; ?>
-<?php require_once __DIR__ . '/template-parts/memberg.php'; ?>
-<?php require_once __DIR__ . '/template-parts/programma.php'; ?>
+<div class="container--bg"></div>
+<div class="wrapper">
+  <?php require_once __DIR__ . '/template-parts/home-intro.php'; ?>
+  <?php require_once __DIR__ . '/template-parts/progetto.php'; ?>
+  <?php require_once __DIR__ . '/template-parts/memberg.php'; ?>
+  <?php require_once __DIR__ . '/template-parts/programma.php'; ?>
+</div>
 <?php get_footer(); ?>
