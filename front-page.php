a165a7c54c2838910e44e98a5ae2793f4e883ac0
serii <seriiburduja@gmail.com>
2022-07-07 17:28:14 +0300

    feat: front page container for first block

--- front-page.php
+++ front-page.php
@@ -5,8 +5,8 @@
  */
 get_header(); ?>
 <?php require_once __DIR__ . '/template-parts/home-intro.php'; ?>
+<?php require_once __DIR__ . '/template-parts/progetto.php'; ?>
 <div class="container container--bg">
-    <?php require_once __DIR__ . '/template-parts/progetto.php'; ?>
     <?php require_once __DIR__ . '/template-parts/memberg.php'; ?>
 </div>
 <div class="container">
