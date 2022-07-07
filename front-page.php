9c62a89c815687e91893518f376d3807a0b0f409
serii <seriiburduja@gmail.com>
2022-07-07 13:34:25 +0300

    finished: projeto mobile

--- front-page.php
+++ front-page.php
@@ -5,10 +5,10 @@
  */
 get_header(); ?>
 <?php require_once __DIR__ . '/template-parts/home-intro.php'; ?>
-<!-- <div class="container container--bg">
+<div class="container container--bg">
     <?php require_once __DIR__ . '/template-parts/progetto.php'; ?>
-    <?php require_once __DIR__ . '/template-parts/memberg.php'; ?>
-</div> -->
+<!--    </?php require_once __DIR__ . '/template-parts/memberg.php'; ?>-->
+</div>
 <!-- <div class="container">
 <?php require_once __DIR__ . '/template-parts/programma.php'; ?>
 </div> -->
